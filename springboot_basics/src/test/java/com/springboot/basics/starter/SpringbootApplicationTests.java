5330e64a2e52d48a9d909156d5805862cebaf0be
zhoubinbin <18598272696@163.com>
2020-01-14 15:10:33 +0800

    完善目录结构

--- springboot_basics/src/test/java/com/springboot/basics/starter/SpringbootApplicationTests.java
+++ springboot_basics/src/test/java/com/springboot/basics/starter/SpringbootApplicationTests.java
@@ -1,9 +1,12 @@
 package com.springboot.basics.starter;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SpringbootApplication.class)
 class SpringbootApplicationTests {
 
     @Test
